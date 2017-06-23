5b242ee03e47eb456c956f6b5cb3daa283d6b7a4
Luca Bacchi <bacchilu@gmail.com>
2017-06-23 11:39:36 +0200

    Tabs->Spaces

--- dispatcher.js
+++ dispatcher.js
@@ -2,56 +2,56 @@
 
 
 const Dispatcher = (function () {
-	let lastId = 1;
-	let callbacks = {};
-	let isDispatching = false;
-	let currentPayload;
+    let lastId = 1;
+    let callbacks = {};
+    let isDispatching = false;
+    let currentPayload;
 
-	const exec = function (ids) {
-		ids.forEach(function (id) {
-			if (callbacks[id]['state'] === true)
-				throw 'Dispatcher.waitFor(...): Circular dependency detected.'
-			if (callbacks[id]['state'] === false)
-				return;
+    const exec = function (ids) {
+        ids.forEach(function (id) {
+            if (callbacks[id]['state'] === true)
+                throw 'Dispatcher.waitFor(...): Circular dependency detected.'
+            if (callbacks[id]['state'] === false)
+                return;
 
-			callbacks[id]['state'] = true;
-			callbacks[id]['cb'](currentPayload);
-			callbacks[id]['state'] = false;
-		});
-	};
+            callbacks[id]['state'] = true;
+            callbacks[id]['cb'](currentPayload);
+            callbacks[id]['state'] = false;
+        });
+    };
 
-	return {
-		register: function (callback) {
-			const id = 'ID_' + lastId++;
-			callbacks[id] = {cb: callback, state: null};
-			return id;
-		},
-		unregister: function (id) {
-			delete callbacks[id];
-		},
-		dispatch: function (payload) {
-			if (isDispatching === true)
-				throw 'Dispatch.dispatch(...): Cannot dispatch in the middle of a dispatch.';
+    return {
+        register: function (callback) {
+            const id = 'ID_' + lastId++;
+            callbacks[id] = {cb: callback, state: null};
+            return id;
+        },
+        unregister: function (id) {
+            delete callbacks[id];
+        },
+        dispatch: function (payload) {
+            if (isDispatching === true)
+                throw 'Dispatch.dispatch(...): Cannot dispatch in the middle of a dispatch.';
 
-			isDispatching = true;
-			for (let id in callbacks)
-				callbacks[id]['state'] = null;
-			currentPayload = payload;
+            isDispatching = true;
+            for (let id in callbacks)
+                callbacks[id]['state'] = null;
+            currentPayload = payload;
 
-			exec(Object.keys(callbacks));
+            exec(Object.keys(callbacks));
 
-			isDispatching = false;
-		},
-		idDispatching: function () {
-			return isDispatching;
-		},
-		waitFor: function (ids) {
-			if (isDispatching === false)
-				throw 'Dispatcher.waitFor(...): Must be invoked while dispatching.'
+            isDispatching = false;
+        },
+        idDispatching: function () {
+            return isDispatching;
+        },
+        waitFor: function (ids) {
+            if (isDispatching === false)
+                throw 'Dispatcher.waitFor(...): Must be invoked while dispatching.'
 
-			exec(ids);
-		}
-	};
+            exec(ids);
+        }
+    };
 })();
 
 
--- main.js
+++ main.js
@@ -6,16 +6,16 @@ const Dispatcher = require('./dispatcher.js');
 let id;
 
 Dispatcher.register(function (payload) {
-	console.log('Func1', payload);
+    console.log('Func1', payload);
 });
 
 Dispatcher.register(function (payload) {
-	Dispatcher.waitFor([id]);
-	console.log('Func2', payload);
+    Dispatcher.waitFor([id]);
+    console.log('Func2', payload);
 });
 
 id = Dispatcher.register(function (payload) {
-	console.log('Func3', payload);
+    console.log('Func3', payload);
 });
 
 
