a690c6006ff19a497cf544a80a06db8e49c2cbce
Luca Bacchi <bacchilu@gmail.com>
2017-06-23 11:36:13 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+const Dispatcher = require('./dispatcher.js');
+
+let id;
+
+Dispatcher.register(function (payload) {
+	console.log('Func1', payload);
+});
+
+Dispatcher.register(function (payload) {
+	Dispatcher.waitFor([id]);
+	console.log('Func2', payload);
+});
+
+id = Dispatcher.register(function (payload) {
+	console.log('Func3', payload);
+});
+
+
+Dispatcher.dispatch({type: 'TEST', value: 'Hello, World!'});
